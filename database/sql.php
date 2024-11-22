23eb7c76a433892a029125ea21215eee240299e1
Vanessa Aguiar <vanessabaraujo17@gmail.com>
2024-11-22 18:15:44 -0300

    Adicionando arquivo index e conexao com banco de dados

---
+++ database/sql.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "12345678";
+
+// Create connection
+$conn = mysqli_connect($servername,$username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
